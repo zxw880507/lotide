d01039acd6eb7d5072f954bd36f510ec8378a13d
Xiaowen Zheng <zxw880507@gmail.com>
2020-09-18 23:11:09 +0000

    eqArray.js updated

--- eqArray.js
+++ eqArray.js
@@ -1,16 +1,33 @@
 // function arrayEqual
+// const eqArrays = function(array1, array2) {
+//   if (array1.length === array2.length) {
+//     for (let i = 0; i < array1.length; i++) {
+//       if (array1[i] !== array2[i]) {
+//         return false;
+//       }
+//     }
+//     return true;
+//   }
+//   return false;
+// };
 const eqArrays = function(array1, array2) {
   if (array1.length === array2.length) {
     for (let i = 0; i < array1.length; i++) {
       if (array1[i] !== array2[i]) {
-        return false;
+        if (Array.isArray(array1[i]) || Array.isArray(array2[i])) {
+          if (!eqArrays(array1[i], array2[i])) {
+            return false;
+          }
+        } else {
+          return false;
+        }
       }
     }
     return true;
+  } else {
+    return false;
   }
-  return false;
 };
-
 
 
 // FUNCTION IMPLEMENTATION
@@ -24,3 +41,24 @@ assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
 assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+
+assertEqual(eqArrays([
+  [2, 3],
+  [4]
+], [
+  [2, 3],
+  [4]
+]), true); // => true
+assertEqual(eqArrays([
+  [2, 3],
+  [4]
+], [
+  [2, 3],
+  [4, 5]
+]), false); // => false
+assertEqual(eqArrays([
+  [2, 3],
+  [4]
+], [
+  [2, 3], 4
+]), false); // => false
