b86097c1b86f5d0e6cdd88ba1cc0de625a3e4948
Xiaowen Zheng <zxw880507@gmail.com>
2020-09-21 16:36:20 +0000

    refactor completed

--- assertEqual.js
+++ assertEqual.js
@@ -4,6 +4,4 @@ const assertEqual = function(actual, expected) {
     console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
 };
 
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
+module.exports = assertEqual;
--- head.js
+++ head.js
@@ -2,12 +2,4 @@ const head = function(array) {
   return array[0];
 };
 
-// FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-  actual === expected ? console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`) :
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-};
-
-// TEST CODE
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+module.exports = head;
