6be503c0e071af306bb6e329eca8b8a7af4f0784
Xiaowen Zheng <zxw880507@gmail.com>
2020-09-22 00:53:59 +0000

    ES6 shortening

--- index.js
+++ index.js
@@ -13,18 +13,17 @@ const without = require('./without');
 
 
 
-
 module.exports = {
-  head: head,
-  tail: tail,
-  middle: middle,
-  countLetters: countLetters,
-  countOnly: countOnly,
-  findKey: findKey,
-  findKeyByValue: findKeyByValue,
-  flatten: flatten,
-  letterPositions: letterPositions,
-  map: map,
-  takeUntil: takeUntil,
-  without: without
+    head,
+    tail,
+    middle,
+    countLetters,
+    countOnly,
+    findKey,
+    findKeyByValue,
+    flatten,
+    letterPositions,
+    map,
+    takeUntil,
+    without
 };
